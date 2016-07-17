964448a90c2b2ce31bec70802d29df0e803de987
jquerycods <cizgifilmhikayeleri@gmail.com>
2016-07-18 00:30:04 +0300

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,14 @@
+﻿//<![CDATA[
+setTimeout(function() {
+  var info = document.getElementById("abp-ads");
+  var ads = document.querySelectorAll("ins.adsbygoogle");
+  if ($(ads).height() === 0 ) {
+    info.className = "show";
+  }
+}, 2000)
+function hideabp() {
+  var e = document.getElementById("abp-ads").style.display = "none";
+};
+//]]>
+//http://bloggereklentileri.blogspot.com.tr/p/blogger-eklentileri-ads-reklam.html
+//http://cizgifilmhikayeleri.blogspot.com.tr/p/niloya-hikayeleri.html
